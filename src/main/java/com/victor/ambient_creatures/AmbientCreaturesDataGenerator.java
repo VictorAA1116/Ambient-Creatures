7ec93a767084c547556fa96440ae3e3526f23e4e
Victor Avila <victoraa1116@gmail.com>
2026-06-03 13:18:07 -0400

    Initial commit

---
+++ src/main/java/com/victor/ambient_creatures/AmbientCreaturesDataGenerator.java
@@ -0,0 +1,11 @@
+package com.victor.ambient_creatures;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AmbientCreaturesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
